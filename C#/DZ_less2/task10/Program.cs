3a4a5cf527eb488aef7010be7bac392c6110a263
iigor <iigor@fer.ru>
2023-06-21 11:21:51 +0300

    DZ_less2_task10

---
+++ C#/DZ_less2/task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трехначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int rez = 0;
+
+rez = number / 10 % 10;
+
+Console.Write(rez);
+
