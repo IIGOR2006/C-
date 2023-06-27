461f5ed3c48ab6496f8ff5cf51c82d6425b93679
iigor <iigor@fer.ru>
2023-06-27 15:55:13 +0300

    dz_less3_task19

--- C#/DZ_less3/task19/Program.cs
+++ C#/DZ_less3/task19/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+console.WriteLine("введите пятизначное число");
+int num = Convert.ToInt32(console.ReadLine());
+
+string num = Convert.ToString(num);
+
+void number(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        console.Write($"ваше число {num} - палидром");
+    }
+
+    else
+    console.WriteLine($"Ваше число: {num} - не палиндром");
+
+}
+
+if (num!.Length == 5)
+{
+    number(num);
+}
+else
+console.WriteLine($"введите правильное число")
