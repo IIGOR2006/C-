13f482ef7104d8c418e7f69f57bbec45bfb1dc21
iigor <Phaser4510@gmail.com>
2023-07-04 18:03:52 +0300

    save

--- C#/DZ_less4/task29/Program.cs
+++ C#/DZ_less4/task29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
