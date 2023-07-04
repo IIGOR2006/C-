f471024f4e2dc8cd9274239350e8ebf3f7e64af4
iigor <Phaser4510@gmail.com>
2023-07-04 17:46:27 +0300

    lesson5

--- C#/DZ_less5/task36/Program.cs
+++ C#/DZ_less5/task36/Program.cs
@@ -2,32 +2,33 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-
-Console.Write(«Введите количество элементов массива: «);
-int a = Convert.ToInt32(Console.ReadLine());
-int[] randomArray = new int[a];
-
-void mas(int a)
-{
-for (int i = 0; i < a; i++)
-{
-randomArray[i] = new Random().Next(1,9);
-Console.Write(randomArray[i] + » «);
-}
-
-}
-
-int kol(int[] randomArray)
-{
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
 int sum = 0;
-int i = 0;
-while (i < randomArray.Length)
-{
-sum = sum + randomArray[i];
-i = i + 2;
-}
-return sum;
-}
 
-mas(a);
-Console.Write($»\nCумма элементов, стоящих на нечётных позициях: {kol(randomArray)}»);
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
