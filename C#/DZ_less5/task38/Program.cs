612fa8dbc49ab93477c8020bc7081cf7c9cb406c
iigor <Phaser4510@gmail.com>
2023-07-04 17:57:32 +0300

    les5

--- C#/DZ_less5/task38/Program.cs
+++ C#/DZ_less5/task38/Program.cs
@@ -2,20 +2,44 @@
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
 
-Console.WriteLine("Введите размер массива:");
-int n = Convert.ToInt32(Console.ReadLine()); // Задаём размер массива
-int [] mass  = new int[n];      // Создаём массив
-Random rnd = new Random();      // Создание объекта для генерации чисел
-for (int i = 0; i < n; i++)
-    { mass[i] = rnd.Next(-50, 50);  // Заполнение рандомами
-    Console.Write($" {mass[i]} ");
-}
-int min = 0;
-int max = 0;
-foreach (int i in mass)  // Поиск максимального и минимального значения
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
 {
-    if (min > i) min = i;
-    if (max < i) max = i;
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
 }
-Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
-Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
+
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
