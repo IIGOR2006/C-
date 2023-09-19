18cb4b81c008fd2add80968c9174eab2cc90c007
iigor <iigor@fer.ru>
2023-09-19 14:53:35 +0300

    update_book

--- python/phone_and_DB/phone_book.py
+++ python/phone_and_DB/phone_book.py
@@ -4,13 +4,17 @@ conn = sqlite3.connect('book.db')
 cursor = conn.cursor()
 
 # cursor.execute( 'CREATE TABLE users ( \
-#     id INTEGER PRIMARY KEY, \
+#     id INTEGER PRIMARY KEY AUTOINCREMENT, \
 #     name TEXT, \
-#     phone_number INTEGER, \
 #     address TEXT, \
 #     mail TEXT)')
-        cursor.execute('INSERT INTO users (phone_number) VALUES (?)', (phone))
-        conn.commit()
+
+# cursor.execute( 'CREATE TABLE phones ( \
+#     id INTEGER PRIMARY KEY AUTOINCREMENT, \
+#     id_user INTEGER, \
+#     phone_number TEXT, \
+#     FOREIGN KEY (id_user) REFERENCES users (id) )')
+
 while True:
     
     conn = sqlite3.connect('book.db')
@@ -21,19 +25,37 @@ while True:
         phone = input('Введите имя еонтакта = ')
         address = input('Введите имя еонтакта = ')
         mail = input('Введите имя еонтакта = ')
-        cursor.execute('INSERT INTO users (name, phone_number, address, mail) VALUES (?, ?, ?, ?)', (name, phone, address, mail))
+        cursor.execute('INSERT INTO users (name, address, mail) VALUES (?, ?, ?)', [name, address, mail])
+        conn.commit()
+        nu = cursor.execute( 'SELECT id FROM users WHERE  name= ?', [name]).fetchone()[0]
+        cursor.execute('INSERT INTO phones (id_user, phone_number) VALUES ( ?, ?)', [nu , phone])
         conn.commit()
     
     def all_data():
-        cursor.execute('SELECT * FROM users')
+        cursor.execute('SELECT name, address, mail,phone_number  FROM users,phones')
         all_result = cursor.fetchall()
         print(all_result)
         
     def delet():
         name = input('Введите имя контакта для удаления')
-        cursor.execute('DELETE mail FROM users WHERE name = ?', [name])
+        nu = cursor.execute( 'SELECT id FROM users WHERE  name= ?', [name]).fetchone()[0]
+        cursor.execute('DELETE FROM phones WHERE id_user = ?', [nu])
+        cursor.execute('DELETE FROM users WHERE name = ?', [name])
         conn.commit()
-    
+        
+    def search():
+        name = input('Введите имя контакта для поиска')
+        cursor.execute('SELECT * FROM users WHERE name = ?', [name])
+        all_result = cursor.fetchall()
+        print(all_result)
+        
+    def update():
+        name = input('Введите имя контакта для поиска')
+        cursor.execute('SELECT * FROM users WHERE name = ?', [name])
+        all_result = cursor.fetchall()
+        print(all_result)
+        nu = input('Введите номер контакта для редактирования')
+        cursor.execute('SELECT * FROM users WHERE name = ?', [name])
         
     option = input("ddt = ")
     if option == '1':
@@ -42,5 +64,7 @@ while True:
         all_data()
     if option == '3':
         delet()
+    if option == '4':
+        search()
         
     conn.close()
