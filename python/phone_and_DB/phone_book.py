3dbf9d2e26523110e56397f769d40d64d098eb2a
iigor <iigor@fer.ru>
2023-09-20 17:33:02 +0300

    update_in_book

--- python/phone_and_DB/phone_book.py
+++ python/phone_and_DB/phone_book.py
@@ -21,15 +21,24 @@ while True:
     cursor = conn.cursor()
     
     def add_user():
-        name = input('Введите имя еонтакта = ')
-        phone = input('Введите имя еонтакта = ')
-        address = input('Введите имя еонтакта = ')
-        mail = input('Введите имя еонтакта = ')
+        name = input('Введите имя контакта = ')
+        phone = input('Введите телефон = ')
+        address = input('Введите адресс = ')
+        mail = input('Введите почту = ')
+        
+        cursor.execute('SELECT *  FROM users')
+        all_result = cursor.fetchall()
+        
+        for i in all_result:
+            if i[1] == name:
+                print('Данное имя уже существует. Рекомендуется задать другое имя, для лучшего ориентирования в списке контактов.')
+            
         cursor.execute('INSERT INTO users (name, address, mail) VALUES (?, ?, ?)', [name, address, mail])
         conn.commit()
         nu = cursor.execute( 'SELECT id FROM users WHERE  name= ?', [name]).fetchone()[0]
         cursor.execute('INSERT INTO phones (id_user, phone_number) VALUES ( ?, ?)', [nu , phone])
         conn.commit()
+      
     
     def all_data():
         cursor.execute('SELECT *  FROM users')
@@ -38,36 +47,85 @@ while True:
         all_result2 = cursor.fetchall()
         
         for i in all_result:
-            print(i[0])
+            print()
             buf = i[0]
+            print(i[1], i[2],i[3])
+            count = 0
             for j in all_result2:
                 if buf == j[1]:
-                    num = j[2]
-                    print(num)
-        
-        # print(all_result)
-        # print(all_result2)
+                    buf_num = j[2]
+                    count +=1
+                    print(f'телефон {count} = ',buf_num)
         
     def delete():
         name = input('Введите имя контакта для удаления')
         nu = cursor.execute( 'SELECT id FROM users WHERE  name= ?', [name]).fetchone()[0]
         cursor.execute('DELETE FROM phones WHERE id_user = ?', [nu])
-        cursor.execute('DELETE FROM users WHERE name = ?', [name])
+        cursor.execute('DELETE FROM users WHERE id = ?', [nu])
         conn.commit()
         
     def search():
         name = input('Введите имя контакта для поиска')
         cursor.execute('SELECT * FROM users WHERE name = ?', [name])
         all_result = cursor.fetchall()
-        print(all_result)
+        id_buf = all_result[0]
+        id_buf = id_buf[0]
+        cursor.execute('SELECT * FROM phones WHERE id_user = ?', [id_buf])
+        all_result2 = cursor.fetchall()
+        for i in all_result:
+            print()
+            buf = i[0]
+            print(i[1], i[2],i[3])
+            count = 0
+            for j in all_result2:
+                if buf == j[1]:
+                    buf_num = j[2]
+                    count +=1
+                    print(f'телефон {count} = ',buf_num)
         
     def update():
+        
         name = input('Введите имя контакта для поиска')
+        
         cursor.execute('SELECT * FROM users WHERE name = ?', [name])
         all_result = cursor.fetchall()
-        print(all_result)
-        nu = input('Введите номер контакта для редактирования')
-        cursor.execute('SELECT * FROM users WHERE name = ?', [name])
+        id_buf = all_result[0]
+        id_buf = id_buf[0]
+        
+        cursor.execute('SELECT * FROM phones WHERE id_user = ?', [id_buf])
+        all_result2 = cursor.fetchall()
+        
+        for i in all_result:
+            print()
+            buf = i[0]
+            print(i[1], i[2],i[3])
+            count = 0
+            
+            for j in all_result2:
+                if buf == j[1]:
+                    buf_num = j[2]
+                    count +=1
+                    print(f'телефон {count} = ',buf_num)
+                    
+                    
+        name_new = input('Введите имя контакта = ')
+        phone_new = input('Введите телефон = ')
+        address_new = input('Введите адресс = ')
+        mail_new = input('Введите почту = ')
+        
+        
+        if len(name_new) == 0:
+            for i in all_result:
+                name_new = i[1]
+        if len(address_new) == 0:
+            for i in all_result:
+                address_new = i[2]
+        if len(mail_new) == 0:
+            for i in all_result:
+                mail_new = i[3]
+                
+        cursor.execute('UPDATE users SET name = ?, address = ?, MAIL = ? WHERE id = ? ',[name_new, address_new, mail_new, id_buf])
+        conn.commit()
         
     option = input("ddt = ")
     if option == '1':
@@ -78,5 +136,7 @@ while True:
         delete()
     if option == '4':
         search()
-        
+    if option == '5':
+        update()
+           
     conn.close()
